133fbc92e9d4c5e2af8597684f0d6d8ee255d890
ShimaYusuke <mashi4729@gmail.com>
2015-05-13 14:50:32 +0900

    mutterModelの実装、およびrequire_onceにdirname(__FILE__)を付加
    requireを多重に行った場合、最初の呼び出し元を起点とした相対パスになってしまうため、各自のパスで絶対パスを指定するようにする

--- class/model/mutter_model.php
+++ class/model/mutter_model.php
@@ -1,8 +1,46 @@
 <?php
-require_once('../dao/mutter_dao.php');
+require_once(dirname(__FILE__) . '/../dao/mutter_dao.php');
 
 class MutterModel{
-  private
+  private $mutter_dao; //クラスインスタンス
+  private $data_ary = "";
 
+  function __construct(){
+    $this->mutter_dao = new MutterDao;
+  }
 
+  public function ctrlHtml(){
+    $this->userId();
+    $this->exeDao();
+    return($this->makeHtml());
+  }
+
+  public function userId(){
+    $user = "";
+
+    //何らかの方法でuserを取得
+    $user = "root";
+
+    $this->mutter_dao->setUserId($user);
+  }
+
+  public function exeDao(){
+    $this->mutter_dao->accessDB();
+    $this->data_ary = $this->mutter_dao->getDataAry();
+  }
+
+  public function makeHtml(){
+    $html = "";
+    foreach($this->data_ary as $row){
+      $html .= "<div class='mutter_base'>";
+      $html .= $row["mutter_title"];
+      
+      $html .= "</div>";
+
+    }
+    return($html);
+  }
 }
+
+$mutter_model = new MutterModel;
+echo($mutter_model->ctrlHtml());

184cb4341d9178459bb17ad28286395919747b7d
kudoshinya <shinya.k930@gmail.com>
2015-05-15 13:32:55 +0900

    ログイン時エラーが出ていたので修正した

--- class/model/login_model.php
+++ class/model/login_model.php
@@ -4,35 +4,31 @@ session_start();//セッションを開始
 require_once ('/class/dao/login_select_dao.php');
 
 if(isset($_POST["login_id"]) && isset($_POST["login_pass"])){
-	//ログインDAOから受け取った値を変数に登録
+	//ログインDAOから受け取った値（ユーザが入力した値）を変数に登録
 	$user_id = $_POST["login_id"];
 	$user_pass = $_POST["login_pass"];
 
+	//入力値をデータベースにアクセスし、IDとPASSが一致するものがあるか確認
 	$login = new LoginDAO;
 	$login -> setUserId($user_id);
 	$login -> setUserPass($user_pass);
 	$login -> accessDB();
 
-	//ログイン名・パスワードが適切かどうかを確認
-	if(!empty($login->getReturnAry())){//値が返ってきたら
-		//セッションID・passを記録
-		echo "ログインに成功しました。<br>";
+	if(!empty($login->getReturnAry())){//値が返ってきたら（一致したら）
+		//セッションIDを発行
 		$loginAry=$login->getReturnAry();
 		$_SESSION['user_id'] = $loginAry[0]['user_id'];
 		$_SESSION['user_name'] = $loginAry[0]['user_name'];
-		echo $_SESSION['user_name'];
-		header("Location: my_page.php");
+		//echo "ログインに成功しました。<br>";
+		//echo $_SESSION['user_name'];
+		header("Location: my_page.php");//ログイン後マイページに自動で飛ぶ
 		exit;
 	}else{
-		//セッションID・passを削除
 		echo "ログインに失敗しました。<br>";
 	}
-
 }else{
 	//echo "string";
 }
-
 session_destroy();
-
 ?>
 
--- common/get_user_name.php
+++ common/get_user_name.php
@@ -3,7 +3,7 @@ session_start();
 $user_name = NULL;
 if(isset($_SESSION['user_id'])){
   if($_SESSION['user_id'] != ""){
-    $user_name = $_SESSOIN['user_id'];
+    $user_name = $_SESSION['user_id'];
   }
 }
 

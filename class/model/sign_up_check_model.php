f955f70f3ef1b4d5eabbc13529de68ef1f533245
ShimaYusuke <mashi4729@gmail.com>
2015-05-15 14:19:22 +0900

    sign_up_modelをクラス化

--- class/model/sign_up_check_model.php
+++ class/model/sign_up_check_model.php
@@ -1,36 +1,45 @@
 <?php
 // このモデルはユーザーサイドのajaxからのPOSTで起動する
+$model = new SignUpCheckModel;
+
 if(isset($_POST['user_name'])){
   // ajaxからpostされる
-  $user_name = $_POST['user_name'];
-
-  require_once(dirname(__FILE__) . '/../dao/check_user_name_dao.php');
-
-  $dao = new CheckUserNameDao;
-  $dao->setUserName($user_name);
-  $dao->accessDB();
-  $return_ary = $dao->getReturnAry();
-
-  if(empty($return_ary)){
-    echo('false');
-  } else {
-    echo('true');
-  }
-} else if(isset($_POST['user_name'])){
+  $return = $model->checkUserName($_POST['user_name']);
+  echo($return);
+} else if(isset($_POST['user_email'])){
   // ajaxからpostされる
-  $user_email = $_POST['user_email'];
-
-  require_once(dirname(__FILE__) . '/../dao/check_user_email_dao.php');
-
-  $dao = new CheckUserEmailDao;
-  $dao->setUserEmail($user_email);
-  $dao->accessDB();
-  $return_ary = $dao->getReturnAry();
-
-  if(empty($return_ary)){
-    echo('false');
-  } else {
-    echo('true');
-  }
+  $return = $model->checkUserEmail($_POST['user_email']);
+  echo($return);
 }
 
+class SignUpCheckModel{
+  public function checkUserName($user_name){
+    require_once(dirname(__FILE__) . '/../dao/check_user_name_dao.php');
+
+    $dao = new CheckUserNameDao;
+    $dao->setUserName($user_name);
+    $dao->accessDB();
+    $return_ary = $dao->getReturnAry();
+
+    if(empty($return_ary)){
+      return(false);
+    } else {
+      return(true);
+    }
+  }
+
+  public function checkUserEmail($user_email){
+    require_once(dirname(__FILE__) . '/../dao/check_user_email_dao.php');
+
+    $dao = new CheckUserEmailDao;
+    $dao->setUserEmail($user_email);
+    $dao->accessDB();
+    $return_ary = $dao->getReturnAry();
+
+    if(empty($return_ary)){
+      return(false);
+    } else {
+      return(true);
+    }
+  }
+}

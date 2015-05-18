4074946cbc69fa036d497961ca3936befd98cd9d
kudoshinya <shinya.k930@gmail.com>
2015-05-18 17:07:17 +0900

    トップページの今週の一押しフレーズと人気の投稿（ランキング）をDBから取ってきて表示するようにした。それぞれのリンクを張った

--- parts/index_content.php
+++ parts/index_content.php
@@ -1,49 +1,39 @@
+<?php require_once(dirname(__FILE__) . '/../class/model/mutter_ranking_select_model.php');
+
+$mutter_ranking = new MutterRankingModel;
+$rank_ary = $mutter_ranking -> getRankingAry();
+?>
 	<article id="main_content" class="bottom">
 		<div id="content_wrapper">
 			<section class="item" id="con01">
 				<h3>今週の一押しフレーズ</h3>
 				<div class="top_image">
-					<img src="./images/batman.jpg">
-					<p class="uploader">投稿者: <a href="#">†BattoMan†</a></p>
+					<img src="$rank_ary[0]['MUTTER_IMG']">
+					<p class="uploader">投稿者: <a href="#"><?php echo $rank_ary[0]['USER_ID']; ?></a></p>
 				</div>
 				<div class="top_phrase">
-					<h4>hogehogeバットマンっぽい画像</h4>
-					<p>
-	初カキコ…ども…<br>
-	俺みたいな中3でグロ見てる腐れ野郎、他に、いますかっていねーか、はは<br>
-
-	今日のクラスの会話<br>
-	あの流行りの曲かっこいい　とか　あの服ほしい　とか<br>
-	ま、それが普通ですわな<br>
-
-	かたや俺は電子の砂漠で死体を見て、呟くんすわ<br>
-	it’a true wolrd．狂ってる？それ、誉め言葉ね。<br>
-
-	好きな音楽　eminem<br>
-	尊敬する人間　アドルフ・ヒトラー（虐殺行為はNO）<br>
-
-	なんつってる間に3時っすよ(笑)　あ～あ、義務教育の辛いとこね、これ<br>
-					</p>
+					<h4> <a href="./parts/mutter_content.php?id=1"><?php echo $rank_ary[0]['MUTTER_TITLE']; ?> </a></h4>
+					<p> <a href="./parts/mutter_content.php?id=1"><?php echo $rank_ary[0]['MUTTER_DETAIL']; ?> </a></p>
 				</div>
 				<div class="clear"></div>
 			</section>
 			
 			<section class="item">
-				<h3>注目の投稿</h3>
+				<h3>新着</h3>
 				
 				<div class="micro_content">
 					<img src="./images/sample.png">
-					<p class="uploader">SAMPLE: <a href="#">BattoMan</a></p>
+					<p class="uploader">投稿者： <a href="#">BattoMan</a></p>
 				</div>
 				
 				<div class="micro_content">
 					<p class="micro_content_txt">厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二厨二</p>
-					<p class="uploader">SAMPLE: <a href="#">BattoMan</a></p>
+					<p class="uploader">投稿者： <a href="#">BattoMan</a></p>
 				</div>
 				
 				<div class="micro_content">
 					<img src="./images/sample.png">
-					<p class="uploader">SAMPLE: <a href="#">BattoMan</a></p>
+					<p class="uploader">投稿者： <a href="#">BattoMan</a></p>
 				</div>
 				<div class="clear"></div>
 			</section>
@@ -52,18 +42,19 @@
 				<h3>人気の投稿</h3>
 				
 				<div class="micro_content">
-					<img src="./images/sample.png">
-					<p class="uploader">SAMPLE: <a href="#">BattoMan</a></p>
+					<!--<img src="$ranl_ary[3]['MUTTER_IMG']">-->
+					<p class="micro_content_text"> <a href="./parts/mutter_content.php?id=2"><?php echo $rank_ary[1]['MUTTER_DETAIL'] ?> </a></p>
+					<p class="uploader">投稿者： <a href="#"><?php echo $rank_ary[1]['USER_ID'] ?> </a></p>
 				</div>
 				
 				<div class="micro_content">
-					<img src="./images/sample.png">
-					<p class="uploader">SAMPLE: <a href="#">BattoMan</a></p>
+					<p class="micro_content_text"> <a href="./parts/mutter_content.php?id=3"><?php echo $rank_ary[2]['MUTTER_DETAIL'] ?> </a></p>
+					<p class="uploader">投稿者： <a href="#"><?php echo $rank_ary[2]['USER_ID'] ?></a></p>
 				</div>
 				
 				<div class="micro_content">
-					<img src="./images/sample.png">
-					<p class="uploader">SAMPLE: <a href="#">BattoMan</a></p>
+					<p class="micro_content_text"> <a href="./parts/mutter_content.php?id=4"><?php echo $rank_ary[3]['MUTTER_DETAIL'] ?> </a></p>
+					<p class="uploader">投稿者： <a href="#"><?php echo $rank_ary[3]['USER_ID'] ?></a></p>
 				</div>
 				<div class="clear"></div>
 			</section>

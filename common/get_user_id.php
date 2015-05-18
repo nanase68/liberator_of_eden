90dff4cc43a51cbcbb053128749fae4021e4eb07
takeshi <sci.and.eng@gmail.com>
2015-05-18 15:22:41 +0900

    呟きのお気に入り機能の実装（DBのInsertのみ）、abstract_daoとmutter_favorites_select_daoを微修正

--- common/get_user_id.php
+++ common/get_user_id.php
@@ -3,7 +3,7 @@ session_start();
 $user_id_from_session = NULL;
 if(isset($_SESSION['user_id'])){
   if($_SESSION['user_id'] != ""){
-    $user_id_from_session = $_SESSOIN['user_id'];
+    $user_id_from_session = $_SESSION['user_id'];
   }
 }
 

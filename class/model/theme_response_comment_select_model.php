4cd45151a2b4f9fb2b121fe44bb941c71a1a433a
takeshi <sci.and.eng@gmail.com>
2015-05-19 17:30:52 +0900

    theme_response_commentのモデルを作成（insertとselectのみ、deleteは未実装）

---
+++ class/model/theme_response_comment_select_model.php
@@ -0,0 +1,35 @@
+<?php 
+require_once(dirname(__FILE__) . '/../dao/theme_response_comment_select_dao.php');
+
+class ThemeResponseCommentSelectModel{
+	private $comment_select_dao; //クラスインスタンス
+	private $data_ary = "";
+	private $response_id;
+
+	function __construct(){
+		$this->comment_select_dao = new ThemeResponseCommentSelectDAO;
+	}
+
+	public function getThemeResponseCommentAry(){
+    	$this->comment_select_dao -> setThemeResponseId($this->response_id);
+		$this->exeDao();
+		return($this->data_ary);
+	}
+
+	private function exeDao(){
+		$this->comment_select_dao->accessDB();
+		$this->data_ary = $this->comment_select_dao->getReturnAry();
+	}
+
+	public function setThemeResponseId($response_id){
+		$this->response_id = $response_id;
+	}
+}
+
+/*
+$model = new ThemeResponseCommentSelectModel;
+$response_id = 1;
+$model->setThemeResponseId($response_id);
+echo "unko<br>";
+print_r($model->getThemeResponseCommentAry());
+*/

19859baf9ff470b91a8fbb641a5a5f7bd8e710a9
Hiroaki <hatuusa@gmail.com>
2015-05-19 17:25:16 +0900

    jsの更新とviewの追加

--- js/jquery.raty/jquery.raty.set.js
+++ js/jquery.raty/jquery.raty.set.js
@@ -1,12 +1,24 @@
 $(function(){
+var url = "../../../class/";
 $.fn.raty.defaults.path = "./js/jquery.raty/stars";
-	$('#star').raty({
-		readOnly: true,
+	$('.star').raty({
 		score: function(){
 			return $(this).attr('data');
 		},
 		click: function(score, evt) {
-			alert("星の数"+score);
+			alert($(this).attr("u-value") + "星の数"+score);
+			
+			
+			$ajaxConnection = $.ajax({
+				type:"POST",
+				url: url,
+				data:{
+					"user_id": $(this).attr("u-value"),
+					"response_id": $(this).attr("data-value")
+				}
+			}).done(function(data){
+			}).fail(function(data){
+			});
 		}
 	});
 });

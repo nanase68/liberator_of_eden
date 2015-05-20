2818a1345c37b5a2e8728fa69c3712fc8e0148df
ShimaYusuke <mashi4729@gmail.com>
2015-05-20 15:16:30 +0900

    とりあえずユーザーidの取得を、取得できない場合rootにするようにした

--- common/get_user_id.php
+++ common/get_user_id.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 $user_id_from_session = NULL;
+
+//stub
+$user_id_from_session = 'root';
 if(isset($_SESSION['user_id'])){
   if($_SESSION['user_id'] != ""){
     $user_id_from_session = $_SESSION['user_id'];
